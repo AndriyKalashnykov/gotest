94258ef4c69097e4e4b0a1ba951a980915295d9a
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-07-15 17:33:20 -0400

    updated version

--- hack/generate-changelog.sh
+++ hack/generate-changelog.sh
@@ -4,7 +4,7 @@ PREV_VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
 NEW_HASH=$(git rev-parse --verify HEAD)
 NEXT_TAG=$(cat VERSION)
 
-OUTPUT=$(git log ${PREV_VERSION}..${NEW_HASH} --no-merges --pretty=format:'* [view commit](http://github.com/christopherhein/go-version/commit/%H) %s' --reverse)
+OUTPUT=$(git log ${PREV_VERSION}..${NEW_HASH} --no-merges --pretty=format:'* [view commit](http://github.com/AndriyKalashnykov/gotest/commit/%H) %s' --reverse)
 
 echo "<!-- START ${NEXT_TAG} -->" >> "CHANGELOG.md"
 echo "## ${NEXT_TAG}" >> "CHANGELOG.md"
