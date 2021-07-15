c59e271169c49eb2dd0e9fb56dd0625d73df6a74
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2021-07-15 18:32:23 -0400

    version v0.0.1

--- hack/get-gotest.sh
+++ hack/get-gotest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; cd $SCRIPT_DIR; cd ..; SCRIPT_PARENT_DIR=$(pwd);
 . $SCRIPT_DIR/set-env.sh
