7636a8510ea87265a84b09dbdb5067f8e2687a33
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-10-02 16:29:37 -0400

    fix: updated README and get script
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- hack/get-gotest.sh
+++ hack/get-gotest.sh
@@ -18,7 +18,7 @@ PROJECT=gotest
 
 # ./get-gotest.sh 0.0.1
 # wget https://github.com/AndriyKalashnykov/gotest/releases/download/v0.0.9/gotest_v0.0.9_Linux_x86_64.tar.gz
-# curl -sfL  https://raw.githubusercontent.com/AndriyKalashnykov/gotest/master/hack/get-gotest.sh | bash -s -- v0.0.9
+# curl -sfL  https://raw.githubusercontent.com/AndriyKalashnykov/gotest/master/hack/get-gotest.sh | bash -s -- 0.0.9
 
 sudo -v
 
@@ -42,6 +42,7 @@ if [ "${VERSION_TO_INSTALL}" != "${VERSION_INSTALLED}" ]; then
         echo "Replacing $PROJECT: $VERSION_INSTALLED > $VERSION_TO_INSTALL"
     fi
 
+    echo "https://github.com/$USER/$PROJECT/releases/download/v${VERSION_TO_INSTALL}/${PROJECT}-v${VERSION_TO_INSTALL}-${OS_LC}-${ARCH}${TAR_GZ_EXT}"
     curl -sSLf "https://github.com/$USER/$PROJECT/releases/download/v${VERSION_TO_INSTALL}/${PROJECT}-v${VERSION_TO_INSTALL}-${OS_LC}-${ARCH}${TAR_GZ_EXT}" | sudo tar -zx -C $BIN_DIR ${PROJECT}
   else
     echo "$PROJECT $VERSION_TO_INSTALL already installed"
