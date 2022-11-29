3c74f767d6eec2f018a12a9f8e7d20f25047dab3
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-11-29 13:20:02 -0500

    fix: removed unused directory
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- completions/gotest.zsh
+++
@@ -1,3 +0,0 @@
-gotest - a Golang POC playground
-
-&main.FieldsByTag{TagName:"validate", Required:[]string{"Address", "NetworkID"}, Optional:[]string{"NodeID", "PoolID"}, OptionalAssociations:[]string{"ToTransactions", "FromTransactions"}}\n
